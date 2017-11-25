ef8b5a3e94f05223d05496a5a79571589c99a05f
Steve Regester <sregester@synack.com>
2017-11-24 18:34:41 -0800

    Play around a bit

--- stories/index.js
+++ stories/index.js
@@ -4,7 +4,7 @@ import { action } from '@storybook/addon-actions'
 
 import AudioVisualiser from '../src/components/AudioVisualiser'
 import AudioAnalyser from '../src/components/AudioAnalyser'
-import audio from '../assets/Sinnplex.mp3'
+import audio from '../assets/childish-gambino-redbone.mp3'
 
 import {
   XYPlot,
@@ -28,16 +28,26 @@ storiesOf('AudioAnalyser', module).add('Basic Usage', () => (
   <AudioVisualiser src={ audio }
     renderFrequencyData={ frequencyData => {
       const oneThirdLength = frequencyData.length / 3
-      const middleThird = frequencyData.slice(0, -oneThirdLength)
-      const data = middleThird.map((y, x) => ({ x, y }))
+      const lowerTwoThirds = frequencyData.slice(0, -oneThirdLength)
+      const data = lowerTwoThirds.map((y, x) => ({ x, y }))
 
       return (
         <XYPlot width={ 600 } height={ 300 }>
-          <LineSeries data={ data } />
-          <XAxis />
-          <YAxis />
+          <LineSeries data={ data } color="purple" />
         </XYPlot>
       )
     } }
   />
 ))
+
+// const chunkedData = []
+// while (lowerThird.length) {
+//   chunkedData.push(lowerThird.splice(0, 4))
+// }
+
+// const data = chunkedData.map((chunk, x) => {
+//   const y = chunk.reduce((memo, amplitude) => {
+//     return memo + amplitude
+//   }, 0) / chunk.length
+//   return { x, y }
+// })
